7a20a1b45543c1776b1c0f2e21a565a5ece18c81
Godwinjose <godwinjoshwa.s@colanonline.com>
2022-09-30 16:20:35 +0530

    map work

--- App.js
+++ App.js
@@ -1,45 +1,78 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useRef} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import MapView, {Marker, PROVIDER_DEFAULT} from 'react-native-maps';
-
+import MapViewDirections from 'react-native-maps-directions';
 
 const App = () => {
-    const [info, setInfo] = React.useState(0);
-    Geolocation.getCurrentPosition(data => {
-        setInfo(data.coords.latitude);
-        console.log(data);
-    });
-    return (
-        <View style={styles.container}>
-            <Text>My App</Text>
-            <Text>My Location - {info}</Text>
-            {/* <MapView
-                 showsUserLocation={true}
-                 provider={PROVIDER_DEFAULT}
-                style={{width: '100%', height: '100%'}}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            >
-              <Marker
-          coordinate={{latitude: 37.7825259, longitude: -122.4351431}}
-          title="Latitude"></Marker>
-      </MapView> */}
-       
-        </View>
-    )
-}
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyA5R-ajr7JfuwD4KY_c7Yu3dYHTX3K6ZMg';
+  const [state, setState] = React.useState({
+    pickupCords: {
+      latitude: 30.7246,
+      longitude: 76.8179,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    droplocationCords: {
+      latitude: 30.7333,
+      longitude: 76.7794,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  });
+  const mapRef = useRef();
+  const {pickupCords, droplocationCords} = state;
 
-export default App
+  const [info, setInfo] = React.useState(0);
+  Geolocation.getCurrentPosition(data => {
+    setInfo(data.coords.latitude);
+    console.log(data);
+  });
+  return (
+    <View style={styles.container}>
+      <Text>My App</Text>
+      <Text>My Location - {info}</Text>
+      <MapView
+        ref={mapRef}
+        showsUserLocation={true}
+        provider={PROVIDER_DEFAULT}
+        style={StyleSheet.absoluteFillObject}
+        initialRegion={pickupCords}>
+        <Marker coordinate={pickupCords} title="start" />
+        <Marker coordinate={droplocationCords} title="destination" />
+
+        <MapViewDirections
+          origin={pickupCords}
+          destination={droplocationCords}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="hotpink"
+          optimizeWaypoints={true}
+          onReady={result => {
+            mapRef.current.fitToCoordinates(result.coordinates, {
+              edgePadding: {
+                right: 30,
+                bottom: 300,
+                left: 30,
+                top: 100,
+              },
+            });
+          }}
+        />
+        {/* <Marker
+          coordinate={{latitude: 37.7825259, longitude: -122.4351431}}
+          title="Latitude"></Marker> */}
+      </MapView>
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
